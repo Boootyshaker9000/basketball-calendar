6c10091d8b604d4c921cca4dbf1a813729e4a59d
Zdeněk Relich <relich@post.cz>
2025-06-03 00:43:15 +0200

    6. Pokus. Schizo.

--- obj/Debug/net8.0-windows/basketball-calendar.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/basketball-calendar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basketball-calendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8197d66ee6cebdf915723c7da41083fac9542941")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbe75375fdf37bf802ff5835630560b15e8de54")]
 [assembly: System.Reflection.AssemblyProductAttribute("basketball-calendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basketball-calendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
